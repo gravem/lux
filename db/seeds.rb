a6078b576d5a740eb0f40af3b7a5e108c18e5075
Aasmund Gravem <aasmund.gravem@gmail.com>
2023-05-29 14:02:14 +0100

    Lux rails app set up with Device and models for car, user and booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+puts 'Creating 10 test users.'
+usernumber = 1
+10.times do
+  user = User.create!(
+    email: "user#{usernumber}@test.com",
+    password: "123456",
+    first_name: "Name#{usernumber}",
+    last_name: "Name"
+  )
+  usernumber += 1
+end
+puts 'Finished!'
