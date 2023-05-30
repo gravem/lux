cbb13482fb350d4c35e2bd241a3f6e77bc0db3c8
Sam Mayhew <mayhew.sam@yahoo.com>
2023-05-30 14:56:00 +0100

    changes based on feedback from pull request made

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,17 @@
 class BookingsController < ApplicationController
-  def new
-    @booking = Booking.new
-  end
-
   def create
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
     if @booking.save
       redirect_to @booking
     else
-      render 'new'
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :car_id, :user_id)
+    params.require(:booking).permit(:start_date, :end_date, :car_id)
   end
 end
